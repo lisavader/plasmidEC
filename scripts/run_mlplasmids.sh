ad12aae4b74721b458e14062425667ea3c79b384
Lisa Vader <lisa@mmvinder.nl>
2021-07-22 17:08:15 +0200

    install and run mlplasmids

---
+++ scripts/run_mlplasmids.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#create output directory
+mkdir -p ../results/mlplasmids_predictions
+
+#clone mlplasmids
+mkdir -p ../tools
+cd ../tools
+if [[ ! -d mlplasmids ]]; then
+	git clone https://gitlab.com/sirarredondo/mlplasmids.git
+else
+	echo "Mlplasmids is already installed."
+fi
+cd mlplasmids
+
+run_mlplasmids(){
+#check whether input directory exists
+[ ! -d ../../$1 ] && exit 1
+#run mlplasmids on all strains in input directory
+for strain in ../../$1/*.fna
+do
+name=$(basename $strain .fna)
+echo "Running mlplasmids on" $name
+Rscript scripts/run_mlplasmids.R $strain ../../results/mlplasmids_predictions/${name}.tsv 1e-5 'Escherichia coli'
+done
+}
+
+while getopts :i:f: flag; do
+	case $flag in
+		i) path=$OPTARG;;
+	esac
+done
+
+#check if input is present
+[ -z $path ] && exit 1
+
+run_mlplasmids $path
