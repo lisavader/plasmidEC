979bf342cdc925b54ed515e0495200a5bd91f0b1
Lisa Vader <lisa@mmvinder.nl>
2021-07-23 19:04:29 +0200

    download database and run platon

--- scripts/run_platon.sh
+++ scripts/run_platon.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
 #make results directory
-mkdir ../results/platon_predictions
+mkdir -p ../results/platon_predictions
 
 #download database
 mkdir -p ../databases/platon
 cd ../databases/platon
-if [[ ! d db ]]; then
+if [[ ! -d db ]]; then
 	wget https://zenodo.org/record/4066768/files/db.tar.gz
 	tar -xzf db.tar.gz
 	rm db.tar.gz
 fi
 
 run_platon(){
-cd ../results/platon_predictions
+cd ../../results/platon_predictions
 #check whether input directory exists
 [ ! -d ../../$1 ] && exit 1
 #run platon on all strains in input directory
 for strain in ../../$1/*.fna
 do
 name=$(basename $strain .fna)
+echo $name
 platon --db ../../databases/platon/db --output $name --threads 8 $strain
 done
 }
+
+while getopts :i: flag; do
+	case $flag in
+		i) path=$OPTARG;;
+	esac
+done
+
+#check if input is present
+[ -z $path ] && exit 1
+
+run_platon $path
