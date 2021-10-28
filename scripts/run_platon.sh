4c98acc330d3b65eca5fabbebd4ba1492506ed98
Lisa Vader <lisa@mmvinder.nl>
2021-10-28 14:55:21 +0200

    Add option to run platon in parallel

--- scripts/run_platon.sh
+++ scripts/run_platon.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-while getopts :i:o: flag; do
+while getopts :i:o:p flag; do
         case $flag in
                 i) path=$OPTARG;;
                 o) output_directory=$OPTARG;;
+		p) parallel=TRUE
         esac
 done
 
@@ -26,9 +27,12 @@ fi
 run_platon(){
 path=$1
 output_directory=$2
+
 cd ../../$output_directory/platon_predictions
+
 #check whether input directory exists
 [ ! -d ../../$path ] && exit 1
+
 #run platon on all strains in input directory
 for strain in ../../$path/*.fasta
 do
@@ -38,4 +42,37 @@ platon --db ../../databases/platon/db --output $name --threads 8 $strain
 done
 }
 
-run_platon $path $output_directory
+run_platon_parallel(){
+path=$1
+output_directory=$2
+
+cd ../../$output_directory
+
+#check whether input directory exists
+[ ! -d ../$path ] && exit 1
+
+#build scripts
+mkdir -p platon_scripts
+
+for strain in ../$path/*.fasta
+do
+name=$(basename $strain .fasta)
+echo "#!/bin/bash
+cd ../platon_predictions
+platon --db ../../databases/platon/db --output $name --threads 8 ../$strain" > platon_scripts/${name}.sh
+done
+
+#execute scripts
+cd platon_scripts
+for script in $(ls *.sh); do
+echo "Running platon script" ${script}"..."
+sbatch --time 1:00:00 --mem 5G -c 8 $script
+done
+}
+
+
+if [[ $parallel = TRUE ]]; then
+	run_platon_parallel $path $output_directory
+else	
+	run_platon $path $output_directory
+fi
