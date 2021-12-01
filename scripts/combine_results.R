27ee85218f2b392be7e431f6a9f2eef6bc6ec66d
Lisa Vader <lisa@mmvinder.nl>
2021-12-01 18:38:38 +0100

    change name of output file

--- scripts/combine_results.R
+++ scripts/combine_results.R
@@ -59,7 +59,7 @@ combined$classification <- "chromosome"
 combined$classification[combined$plasmid_count>1] <- "plasmid"
 
 ##Write output file
-combined_path<-paste('../',output_directory,'/final_output.csv',sep='')
+combined_path<-paste('../',output_directory,'/plasmidEC_output.csv',sep='')
 write.csv(combined,combined_path,row.names = FALSE)
 
 ##write gplas output
