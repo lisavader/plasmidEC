1ec463cddb3f03ea3ae952017d8fbf34697fb17f
Julian Paganini <j.a.paganini@umcutrecht.nl>
2022-06-07 06:13:57 +0200

    Fix issue with if statement and put variables into quotes

--- scripts/gather_results.sh
+++ scripts/gather_results.sh
@@ -19,8 +19,8 @@ do
 if [ -f $out_dir/mlplasmids_output/$file.tsv ]; then
 tail -n +2 $out_dir/mlplasmids_output/$file.tsv | while read line
 do
-prediction=$(echo $line | cut -d' ' -f3 | sed 's/"//g')
-contig=$(echo $line | cut -d' ' -f4 | sed 's/"//g')
+prediction=$(echo "$line" | cut -f3 | sed 's/"//g')
+contig=$(echo "$line" | cut -f4 | sed 's/"//g')
 echo $contig,${prediction,,},mlplasmids,$file >> $out_dir/all_predictions.csv
 done
 else
@@ -32,13 +32,12 @@ done
 ##PLASCOPE
 gather_plascope(){
 results_dir=$out_dir/plascope_output/*_PlaScope
-
 #go to extended result file to find contig ID and class nr
 if [ -f $results_dir/Centrifuge_results/*extendedresult ]; then
 cat $results_dir/Centrifuge_results/*extendedresult | sed '1d' | while read line
 do
-contig=$(echo $line | cut -d ' ' -f1)
-class_nr=$(echo $line | cut -d ' ' -f3)
+contig=$(echo "$line" | cut -f1)
+class_nr=$(echo "$line" | cut -f3)
 #translate class nr to classification (i.e. 2 = chromosome, 3 = plasmid, 0 / 1 = unclassified)
 if [ $class_nr == '3' ]; then
 	classification="plasmid"
@@ -46,12 +45,12 @@ elif [ $class_nr == '2' ]; then
 	classification="chromosome"
 elif [ $class_nr == '0' ] || [ $class_nr == '1' ]; then
 	classification="unclassified"
-else
-echo "PlaScope output couldn't be found. Check ${results_dir}/Centrifuge_results/centrifuge.log for issues" && exit 1
 fi
 #write to file
 echo $contig,$classification,"plascope",$file >> $out_dir/all_predictions.csv
 done
+else
+echo "PlaScope output couldn't be found. Check ${results_dir}/Centrifuge_results/centrifuge.log for issues" && exit 1
 fi
 }
 
@@ -62,13 +61,13 @@ results_dir=$out_dir/platon_output/*
 if [ -f $results_dir/*chromosome.fasta ]; then
 cat $results_dir/*chromosome.fasta | grep '>' | while read line
 do
-contig=$(echo $line | cut -c 2-)
+contig=$(echo "$line" | cut -c 2-)
 echo $contig,"chromosome","platon",$file >> $out_dir/all_predictions.csv
 done
 #grab plasmid contigs
 cat $results_dir/*plasmid.fasta | grep '>' | while read line
 do
-contig=$(echo $line | cut -c 2-)
+contig=$(echo "$line" | cut -c 2-)
 echo $contig,"plasmid","platon",$file >> $out_dir/all_predictions.csv
 done
 else
@@ -82,8 +81,8 @@ dir=$(ls -Art $out_dir/rfplasmid_output | tail -n 1)
 if [ -f $out_dir/rfplasmid_output/$dir/prediction.csv ]; then 
 tail -n +2 $out_dir/rfplasmid_output/$dir/prediction.csv | while read line
 do
-#file=$(echo $line | cut -f 1 -d ',' | awk -F '_' 'BEGIN { OFS = FS }; NF { NF -= 1 }; 1' | sed 's/"//g')
-contig=$(echo $line | cut -d, -f5 | sed 's/"//g')
+#file=$(echo "$line" | cut -f 1 -d ',' | awk -F '_' 'BEGIN { OFS = FS }; NF { NF -= 1 }; 1' | sed 's/"//g')
+contig=$(echo "$line" | cut -d, -f5 | sed 's/"//g')
 if [[ $line = *'"p"'* ]]; then
 echo $contig,"plasmid","rfplasmid",$file >> $out_dir/all_predictions.csv
 else
